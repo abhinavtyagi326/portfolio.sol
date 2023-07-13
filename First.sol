139f747c4e6905c7b9cdf2bca7cc5051fb4434ea
abhinavtyagi326 <abhinavtyagi471@gmail.com>
2023-07-13 17:13:13 +0530

    Initial commit: remix template remixDefault

---
+++ First.sol
@@ -0,0 +1,103 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract First
+{   
+    // sample program
+    // string public name ;
+    // uint age;
+    // constructor() 
+    // {
+    //     name = "Ravi";
+    //     age = 17;
+    // }
+    // function getName() view public returns(string memory){
+    //     return name;
+    // }
+    // function getAge() view public returns(uint){
+    //     return age;
+    // }
+    // function setAge() public {
+    //    age = age+1;
+    // }
+
+
+
+    // state variable
+    // uint public age;
+    // method 1
+    // constructor(){
+    //     age=10;
+    // }
+    // method 2
+    // uint public age=10;
+    // method 3
+    // function set() public {
+    //     age=10;
+    // }
+
+
+
+    // local variable
+    // function store() pure public returns (string memory){
+    //     string memory name = "Abhinav";
+    //     uint age=20; 
+    //     return name;
+    // }
+
+
+
+    // function without argument
+    // uint age = 10;
+    // function setter() public{
+    //     age+=1;
+    // }
+    // function getter() public view returns(uint){
+    //     return age;
+    // } 
+
+
+
+    // function with argument
+    // uint age = 10;
+    // function setter(uint newage) public{
+    //     age = newage;
+    // }
+    // function getter() view public  returns(uint){
+    //     return age;
+    // }
+
+
+
+    // uint public age=10;
+    // constructor(uint ag){
+    //     age=ag;
+    // }
+
+
+
+    // uint[4] public arr=[19,10,1222,5];
+    // function setter(uint index,uint value) public{
+    //     arr[index] = value;
+    // }
+    // function length() public view returns (uint){
+    //     return arr.length;
+    // }
+
+
+
+    // uint[] public arr;
+    // function pushelem(uint item) public {
+    //     arr.push(item);
+    // }
+    // function len() public view returns (uint){
+    //     return arr.length;
+    // }
+    // function popelem() public{
+    //     arr.pop();
+    // }
+
+
+    
+
+}
